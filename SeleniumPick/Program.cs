698fb8efae3563e4685c9b2e2072972475c4d38b
jtueros <Josue.Tueros@cignium.com>
2019-01-22 10:59:00 -0500

    Busqueda en Wikipedia

--- SeleniumPick/Program.cs
+++ SeleniumPick/Program.cs
@@ -16,34 +16,58 @@ namespace SeleniumPick
     {
         static void Main(string[] args)
         {
+
+
+
+
+            //IWebDriver _driver = new ChromeDriver();
+            //_driver.Manage().Window.Maximize();
+            //_driver.Url = "https://duckduckgo.com/";
+
+            //var txtSearch = _driver.FindElement(By.Id("search_form_input_homepage"));
+            //Console.WriteLine("escribir algo");
+            //txtSearch.Clear();
+            //txtSearch.SendKeys("invoker dota 2");
+
+            //var btnSearch = _driver.FindElement(By.Id("search_button_homepage"));
+            //btnSearch.Click();
+            //Task.Delay(3500).Wait();
+
+
+            //var btnImagenes = _driver.FindElement(By.ClassName("js-zci-link--images"));
+            //btnImagenes.Click();
+            //Task.Delay(3500).Wait();
+
+            //var primeraImagen = _driver.FindElement(By.ClassName("zci__main"))
+            //                   .FindElements(By.ClassName("tile--img"))[0];
+            //primeraImagen.Click();
+            //Task.Delay(3500).Wait();
+
+
+            //Screenshot ss = ((ITakesScreenshot)_driver).GetScreenshot();
+            //ss.SaveAsFile("D:\\imgs\\xddx2.png", ScreenshotImageFormat.Png);
+
+            //_driver.Quit();
+
+
             IWebDriver _driver = new ChromeDriver();
             _driver.Manage().Window.Maximize();
-            _driver.Url = "https://duckduckgo.com/";
+            _driver.Url = "https://es.wikipedia.org/wiki/Wikipedia:Portada";
 
-            var txtSearch = _driver.FindElement(By.Id("search_form_input_homepage"));
-            Console.WriteLine("escribir algo");
+            var txtSearch = _driver.FindElement(By.Id("searchInput"));
+            Console.WriteLine("buscar algo");
             txtSearch.Clear();
-            txtSearch.SendKeys("invoker dota 2");
+            txtSearch.SendKeys("Peru");
 
-            var btnSearch = _driver.FindElement(By.Id("search_button_homepage"));
+            var btnSearch = _driver.FindElement(By.Id("searchButton"));
             btnSearch.Click();
             Task.Delay(3500).Wait();
 
-
-            var btnImagenes = _driver.FindElement(By.ClassName("js-zci-link--images"));
-            btnImagenes.Click();
-            Task.Delay(3500).Wait();
-
-            var primeraImagen = _driver.FindElement(By.ClassName("zci__main"))
-                               .FindElements(By.ClassName("tile--img"))[0];
-            primeraImagen.Click();
-            Task.Delay(3500).Wait();
-
-
             Screenshot ss = ((ITakesScreenshot)_driver).GetScreenshot();
-            ss.SaveAsFile("D:\\imgs\\xddx2.png", ScreenshotImageFormat.Png);
-            
+            ss.SaveAsFile("D:\\imgs\\Peru.png", ScreenshotImageFormat.Png);
+
             _driver.Quit();
+
         }
     }
 }
